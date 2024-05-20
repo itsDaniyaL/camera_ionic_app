c5ab4e9676ba4a18104f5ac735ec1fb31e65c414
waqas.ede <waqas.ede@gmail.com>
2024-05-21 01:25:47 +0500

    Fixed camrea previw module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CameraPreview } from '@awesome-cordova-plugins/camera-preview/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },CameraPreview],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
