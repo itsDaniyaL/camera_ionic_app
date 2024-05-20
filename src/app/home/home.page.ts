265007a658bddd6ac7b253808dfcd5b75029e2f4
Daniyal Ahmad Rizwan <68680852+itsDaniyaL@users.noreply.github.com>
2024-05-20 07:49:53 -0400

    added viewfinder edit buttons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  editIconActive = false;
 
   constructor() {}
+
+  onClick() {
+    this.editIconActive = !this.editIconActive;
+  }
 
 }
