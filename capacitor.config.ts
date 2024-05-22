7c4178c7bce4b39927cf99e2386cc155536d47d6
waqas.ede <waqas.ede@gmail.com>
2024-05-22 15:26:41 +0500

    update 001

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'camera_app',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    url: "http://192.168.10.14:8100",
+    cleartext: true
+  }
 };
 
 export default config;
