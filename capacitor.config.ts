0cf11279eb1169e6066a6cf2e51963457ca4e6f8
Waqas Ahmad <92909090+WaqasAhmad-EDE@users.noreply.github.com>
2024-05-25 01:22:17 +0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,10 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'camera_app',
   webDir: 'www',
-  server: {
-    url: "http://192.168.10.14:8100",
-    cleartext: true
-  }
+  // server: {
+  //   url: "http://192.168.10.14:8100",
+  //   cleartext: true
+  // }
 };
 
 export default config;
