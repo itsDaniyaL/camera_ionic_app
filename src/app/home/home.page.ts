9502aa2e09adbd04bd35a8eba162aafc8606e018
waqas.ede <waqas.ede@gmail.com>
2024-05-30 04:44:41 +0500

    removed un ness code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -39,61 +39,13 @@ export class HomePage {
   }
 
   isCameraStarted = false;
+  imageURL: string | null | undefined = null
 
   constructor(
-    // private cameraPreview: CameraPreview,
-  ) {
-    setTimeout(() => {
-      CameraPreview.stop();
-    }, 1000);
-  }
-
-  // openCamera() {
-  //   this.isCameraStarted = true;
-  //   this.startCamera();
-  // }
-  // async startCamera() {
-  //   let w = window.screen.width;
-  //   let h = window.screen.height;
-  //   let options: CameraPreviewOptions = {
-  //     height: h,
-  //     width: w,
-  //     camera: "rear",
-  //     storeToFile: false,
-  //     toBack: true
-  //   };
-  //   // Start Camera
-  //   const camera = await this.cameraPreview.startCamera(options);
-  //   // await this.cameraPreview.setFlashMode(this.cameraPreview.FLASH_MODE.TORCH)
-  //   // alert(JSON.stringify(this.cameraPreview.setFlashMode(this.cameraPreview.FLASH_MODE.TORCH)))
-  //   alert(JSON.stringify(this.cameraPreview.getCameraCharacteristics()))
-
-  //   // For Temp Purpose
-  //   // setTimeout(() => {
-  //   //   this.stopCamera()
-  //   // }, 5000);
-
-  //   // For Flash
-  //   // await this.cameraPreview.setFlashMode(this.cameraPreview.FLASH_MODE.TORCH)
-
-  //   // Camera Zoom
-  //   // await this.cameraPreview.getMaxZoom().then((maxZoom) => {
-  //   //   this.cameraPreview.setZoom(maxZoom * this.cameraSettings.camera_zoom / 100)
-  //   // })
-  // }
-  // stopCamera() {
-  //   this.isCameraStarted = false;
-  //   this.cameraPreview.stopCamera();
-  // }
-  // switchCamera() {
-  //   this.cameraPreview.switchCamera();
-  // }
+  ) { }
 
 
   async openCamera() {
-
-
-
     const takePicture = async () => {
       // setTimeout(() => {
       //   document.getElementsByTagName('pwa-camera-modal-instance')[0].style.display = 'block';
@@ -102,54 +54,17 @@ export class HomePage {
         quality: 90,
         allowEditing: true,
         source: CameraSource.Prompt,
-        resultType: CameraResultType.Uri,
+        resultType: CameraResultType.Base64,
       });
-
-
 
       // image.webPath will contain a path that can be set as an image src.
       // You can access the original file using image.path, which can be
       // passed to the Filesystem API to read the raw data of the image,
       // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
       this.imageURL = image.webPath;
-
-
-      // Can be set to the src of an image now
-      // imageElement.src = imageUrl;
     };
-
-
     await takePicture();
-
-    // alert(window.screen.height)
-    // alert(window.screen.width)
-
-    // this.capturedImage = null
-    // await CameraPreview.start({ width: window.screen.width, height: window.screen.height,parent: "cameraPreview", position: 'front', toBack: true, className: 'camera-video-div' });
-    // this.isCameraStarted = true;
   }
-  imageURL: string | null | undefined = null
-
-  async stopCamera() {
-    await CameraPreview.stop();
-    this.isCameraStarted = false;
-
-  }
-
-  async switchCamera() {
-    await CameraPreview.flip();
-  }
-
-  capturedImage: any = null
-  async takePicture() {
-    const image = await CameraPreview.capture({ width: window.screen.width, height: window.screen.height, quality: 100 });
-    // alert(JSON.stringify(image))
-    this.capturedImage = "data:image/jepg;base64," + image.value
-
-    this.stopCamera()
-  }
-
-
 
   rotateImage = false
   RotateImage() {
